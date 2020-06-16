38eaf0f8aafc57c8b627023902d2a7dde079daec
Alex Roth <alexroth96@gmail.com>
2020-06-16 13:47:07 -0400

    add new project and transforms to images

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,70 +1,71 @@
 export const apps = [
-	{
-		title: 'Italiano Bros Website',
-		tech: 'Built with ReactJS, GatsbyJS, Sass, and NodeJs. ',
-		desc: 'Dynamic and responsive website for a construction company.',
-		img: [
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_53,w_765,h_468/v1567980236/portfolio/2019-09-08_5_ciahbf.png',
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,h_1040,w_525/v1580432102/portfolio/2020-01-30_4_j7czx4.png',
-		],
-		url: 'http://www.italianobrosenterprise.com/',
-		codeurl: 'https://github.com/AlexShiresRoth/italianobrosv2',
-	},
-	{
-		title: 'Fancy Active Wear',
-		tech: 'built with SvelteJS and SCSS.',
-		desc: 'Mock website for a fake clothing brand,',
-		img: [
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_53,w_765,h_468/v1581450719/portfolio/2020-02-11_ii3g55.png',
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_55,w_1126/v1581450533/portfolio/2020-02-11_2_s1g3ff.png',
-		],
-		url: 'https://fancyactivewear.netlify.com/',
-		codeurl: 'https://github.com/AlexShiresRoth/clothingbrandapp',
-	},
+  {
+    title: "Fill The Void Productions",
+    tech: "built with ReactJS, Redux, ThreeJS, Sass, NodeJS.",
+    desc: `Website built for my freelance business,`,
+    img: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_765,h_468/v1592321446/portfolio/Screenshot_2020-06-16_11.22.15_uspv4a.jpg",
+      "https://res.cloudinary.com/snackmanproductions/image/upload/q_56/v1592321518/portfolio/Screenshot_2020-06-16_11.23.43_ii7lur.jpg",
+    ],
+    url: "https://fillthevoid.io/",
+    codeurl: "https://github.com/AlexShiresRoth/arproductions",
+  },
+  {
+    title: "Italiano Bros Website",
+    tech: "Built with ReactJS, GatsbyJS, Sass, and NodeJs. ",
+    desc: "Dynamic and responsive website for a construction company.",
+    img: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_53,w_765,h_468/v1567980236/portfolio/2019-09-08_5_ciahbf.jpg",
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,h_1040,w_525/v1580432102/portfolio/2020-01-30_4_j7czx4.jpg",
+    ],
+    url: "http://www.italianobrosenterprise.com/",
+    codeurl: "https://github.com/AlexShiresRoth/italianobrosv2",
+  },
+  {
+    title: "Fancy Active Wear",
+    tech: "built with SvelteJS and SCSS.",
+    desc: "Mock website for a fake clothing brand,",
+    img: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_53,w_765,h_468/v1581450719/portfolio/2020-02-11_ii3g55.jpg",
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_55,w_1126/v1581450533/portfolio/2020-02-11_2_s1g3ff.jpg",
+    ],
+    url: "https://fancyactivewear.netlify.com/",
+    codeurl: "https://github.com/AlexShiresRoth/clothingbrandapp",
+  },
 
-	{
-		title: 'Progress Journal',
-		tech: 'built with MongoDB Atlas, Sass, Express, Javascript, NodeJS, and PassportJS.',
-		desc: 'Fully functional and responsive CRUD app, ',
-		img: [
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,w_765,h_468/v1580267213/portfolio/2020-01-28_xqfsjd.png',
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_897/v1580442749/portfolio/2020-01-30_8_cupmh8.png',
-		],
-		url: 'https://www.progressjournalsapp.com/',
-		codeurl: 'https://github.com/AlexShiresRoth/progress-journal',
-	},
-	{
-		title: 'Send Noods',
-		tech: 'built with ReactJS and Redux.',
-		desc: `Dynamic and responsive web app for finding ramen restaurants from search. 
+  {
+    title: "Progress Journal",
+    tech:
+      "built with MongoDB Atlas, Sass, Express, Javascript, NodeJS, and PassportJS.",
+    desc: "Fully functional and responsive CRUD app, ",
+    img: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,w_765,h_468/v1580267213/portfolio/2020-01-28_xqfsjd.jpg",
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_897/v1580442749/portfolio/2020-01-30_8_cupmh8.jpg",
+    ],
+    url: "https://www.progressjournalsapp.com/",
+    codeurl: "https://github.com/AlexShiresRoth/progress-journal",
+  },
+  {
+    title: "Send Noods",
+    tech: "built with ReactJS and Redux.",
+    desc: `Dynamic and responsive web app for finding ramen restaurants from search. 
         Integrated with Google Places/Maps API and`,
-		img: [
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_765,h_468/v1575660519/portfolio/2019-12-06_1_awxsjf.png',
-			'https://res.cloudinary.com/snackmanproductions/image/upload/v1580442882/portfolio/2020-01-30_10_dyjupe.png',
-		],
-		url: 'https://send-noods.netlify.com/',
-		codeurl: 'https://github.com/AlexShiresRoth/send-noods',
-	},
-	{
-		title: 'Pedal Dashboard',
-		desc: `Dynamic web app for checking out guitar effects pedals, utilizing the Reverb API and`,
-		tech: 'built with ReactJS and Redux.',
-		img: [
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_765,h_468/v1576456462/portfolio/2019-12-15_2_tv3vop.png',
-			'https://res.cloudinary.com/snackmanproductions/image/upload/v1580443216/portfolio/2020-01-30_14_nsic8m.png',
-		],
-		url: 'https://pedal-dashboard.netlify.com/',
-		codeurl: 'https://github.com/AlexShiresRoth/pedal-dash',
-	},
-	{
-		title: 'Consurgo International, Inc',
-		tech: 'built with ReactJS, GatsbyJS, Sass, NodeJS.',
-		desc: `Responsive SPA for an exporting company,`,
-		img: [
-			'https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_58,w_765,h_468/v1580442667/portfolio/2019-10-29_kzfuqc.png',
-			'https://res.cloudinary.com/snackmanproductions/image/upload/v1580443289/portfolio/2020-01-30_12_xaruxb.png',
-		],
-		url: 'https://consurgointl.com/',
-		codeurl: 'https://github.com/AlexShiresRoth/consurgointl',
-	},
+    img: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_765,h_468/v1575660519/portfolio/2019-12-06_1_awxsjf.jpg",
+      "https://res.cloudinary.com/snackmanproductions/image/upload/v1580442882/portfolio/2020-01-30_10_dyjupe.jpg",
+    ],
+    url: "https://send-noods.netlify.com/",
+    codeurl: "https://github.com/AlexShiresRoth/send-noods",
+  },
+  {
+    title: "Pedal Dashboard",
+    desc: `Dynamic web app for checking out guitar effects pedals, utilizing the Reverb API and`,
+    tech: "built with ReactJS and Redux.",
+    img: [
+      "https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_54,w_765,h_468/v1576456462/portfolio/2019-12-15_2_tv3vop.jpg",
+      "https://res.cloudinary.com/snackmanproductions/image/upload/v1580443216/portfolio/2020-01-30_14_nsic8m.jpg",
+    ],
+    url: "https://pedal-dashboard.netlify.com/",
+    codeurl: "https://github.com/AlexShiresRoth/pedal-dash",
+  },
 ];
