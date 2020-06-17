6168c036cdba9dc9477a530f460cefe0f2cb9304
Alex Rothenberg <alexroth96@gmail.com>
2020-06-17 17:15:09 -0400

    fix target

--- src/components/reusable/Icons.js
+++ src/components/reusable/Icons.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { animated } from "react-spring";
-import style from "../Header.module.scss";
+import React from 'react';
+import { animated } from 'react-spring';
+import style from '../Header.module.scss';
 
 export const Icons = ({ icons, slideUpLong }) => {
-  const renderIcons = icons.map((icon, i) => {
-    return (
-      <a target="_blank" href={`${icon.url}`} key={i}>
-        {icon.icon}
-      </a>
-    );
-  });
+	const renderIcons = icons.map((icon, i) => {
+		return (
+			<a target="_blank" href={`${icon.url}`} key={i} rel="noopener noreferrer">
+				{icon.icon}
+			</a>
+		);
+	});
 
-  return (
-    <animated.div style={slideUpLong} className={style.icons}>
-      {renderIcons}
-    </animated.div>
-  );
+	return (
+		<animated.div style={slideUpLong} className={style.icons}>
+			{renderIcons}
+		</animated.div>
+	);
 };
