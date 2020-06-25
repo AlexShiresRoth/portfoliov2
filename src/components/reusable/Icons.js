ef4fe21fed8cd10fb1d09d048ebf40b2f1035e22
Alex Roth <alexroth96@gmail.com>
2020-06-25 12:42:38 -0400

    take out icon animation

--- src/components/reusable/Icons.js
+++ src/components/reusable/Icons.js
@@ -1,14 +1,7 @@
 import React from "react";
-import { animated, useSpring } from "react-spring";
 import style from "../Header.module.scss";
 
 export const Icons = ({ icons }) => {
-  const slideUpLong = useSpring({
-    config: { duration: 500 },
-    delay: 900,
-    to: { transform: "translateY(0vh)", opacity: 1 },
-    from: { transform: "translateY(30vh)", opacity: 0 },
-  });
   const renderIcons = icons.map((icon, i) => {
     return (
       <a target="_blank" href={`${icon.url}`} key={i} rel="noopener noreferrer">
@@ -18,8 +11,8 @@ export const Icons = ({ icons }) => {
   });
 
   return (
-    <animated.div style={slideUpLong} className={style.icons}>
+    <div style={slideUpLong} className={style.icons}>
       {renderIcons}
-    </animated.div>
+    </div>
   );
 };
