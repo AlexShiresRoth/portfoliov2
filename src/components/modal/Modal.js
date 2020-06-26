c9ecd5838e044a40d1bb6218689d1ae94c5c3d22
Alex Roth <alexroth96@gmail.com>
2020-06-26 09:41:20 -0400

    fix email modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,25 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../../css/main.css";
+import style from "../Contact.module.scss";
 const Modal = ({ response, modalState, handleModalState }) => {
   return modalState ? (
-    <div className={modalState ? "modal__container show" : "modal__container"}>
-      <div className="modal__container--response">
+    <div className={modalState ? `${style.modal} ${style.show}` : style.modal}>
+      <div className={style.response}>
         <p style={{ color: `${response.error ? "#F31431" : "#4BB543"}` }}>
           {response.success
             ? response.success.data.msg
             : `Looks like there was a problem sending the email...`}
         </p>
-        <button onClick={e => handleModalState()}>Close</button>
+        <button onClick={(e) => handleModalState()}>Close</button>
       </div>
     </div>
-  ) : (
-    <></>
-  );
+  ) : null;
 };
 
 Modal.propTypes = {
-  response: PropTypes.object.isRequired
+  response: PropTypes.object.isRequired,
 };
 
 export default Modal;
