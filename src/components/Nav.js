af759d2bb7fc66770ef38414daf345f30e3114ba
Alex <alexroth96@gmail.com>
2021-04-01 15:15:17 -0400

    new logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,99 +1,116 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import smoothscroll from 'smoothscroll-polyfill';
-import style from './Nav.module.scss';
-import { logo2 } from './reusable/logo';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import smoothscroll from "smoothscroll-polyfill";
+import style from "./Nav.module.scss";
+import { logo2, logo3 } from "./reusable/logo";
 
 smoothscroll.polyfill();
 
 const Nav = ({ projectsRef, skillsRef, contactRef, fixedNav, active }) => {
-	const [show, toggle] = useState(false);
+  const [show, toggle] = useState(false);
 
-	const onClick = (e) => {
-		toggle(!show);
-	};
+  const onClick = (e) => {
+    toggle(!show);
+  };
 
-	const navSvg = (
-		<svg viewBox="0 0 100 100">
-			<g>
-				<path d="M 0 50 L100 50 Z" strokeWidth="6px" />
-				<path d="M 0, 30 L100, 30 Z" strokeWidth="6px" />
-				<path d="M 0, 70 L100, 70 Z" strokeWidth="6px" />
-			</g>
-		</svg>
-	);
-	const mobileMenu = (
-		<div className={show ? `${style.mobile_menu} ${style.show}` : `${style.mobile_menu} ${style.hide}`}>
-			<a onClick={() => handleNavClick()} href="#!">
-				Home
-			</a>
-			<a onClick={() => handleNavClick(projectsRef)} href="#!">
-				Projects
-			</a>
-			<a onClick={() => handleNavClick(skillsRef)} href="#!">
-				Skills
-			</a>
-			<a onClick={() => handleNavClick(contactRef)} href="#!">
-				Contact
-			</a>
-		</div>
-	);
+  const navSvg = (
+    <svg viewBox="0 0 100 100">
+      <g>
+        <path d="M 0 50 L100 50 Z" strokeWidth="6px" />
+        <path d="M 0, 30 L100, 30 Z" strokeWidth="6px" />
+        <path d="M 0, 70 L100, 70 Z" strokeWidth="6px" />
+      </g>
+    </svg>
+  );
+  const mobileMenu = (
+    <div
+      className={
+        show
+          ? `${style.mobile_menu} ${style.show}`
+          : `${style.mobile_menu} ${style.hide}`
+      }
+    >
+      <a onClick={() => handleNavClick()} href="#!">
+        Home
+      </a>
+      <a onClick={() => handleNavClick(projectsRef)} href="#!">
+        Projects
+      </a>
+      <a onClick={() => handleNavClick(skillsRef)} href="#!">
+        Skills
+      </a>
+      <a onClick={() => handleNavClick(contactRef)} href="#!">
+        Contact
+      </a>
+    </div>
+  );
 
-	const handleNavClick = (ref) => {
-		toggle(false);
-		window.scrollTo({
-			top: ref ? ref.current.offsetTop - 70 : 0,
-			left: 0,
-			behavior: 'smooth',
-		});
-	};
+  const handleNavClick = (ref) => {
+    toggle(false);
+    window.scrollTo({
+      top: ref ? ref.current.offsetTop - 70 : 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
-	return (
-		<nav className={`${style.nav__components} ${fixedNav ? style.fixed_nav : null}`}>
-			<div className={style.nav__logo}>
-				{logo2}
-				<p>Alex Rothenberg</p>
-			</div>
-			<div className={style.links}>
-				<a onClick={() => handleNavClick()} href="#!" className={active === 'Home' ? style.active : null}>
-					Home
-				</a>
-				<a
-					onClick={() => handleNavClick(projectsRef)}
-					href="#!"
-					className={active === 'Projects' ? style.active : null}
-				>
-					Projects
-				</a>
-				<a
-					onClick={() => handleNavClick(skillsRef)}
-					href="#!"
-					className={active === 'Skills' ? style.active : null}
-				>
-					Skills
-				</a>
-				<a
-					onClick={() => handleNavClick(contactRef)}
-					href="#!"
-					className={active === 'Contact' ? style.active : null}
-				>
-					Contact
-				</a>
-			</div>
-			<div className={show ? `${style.mobile} ${style.rotated}` : style.mobile} onClick={(e) => onClick(e)}>
-				{navSvg}
-			</div>
-			{mobileMenu}
-		</nav>
-	);
+  return (
+    <nav
+      className={`${style.nav__components} ${
+        fixedNav ? style.fixed_nav : null
+      }`}
+    >
+      <div className={style.nav__logo}>
+        {logo3}
+        <p>Alex Rothenberg</p>
+      </div>
+      <div className={style.links}>
+        <a
+          onClick={() => handleNavClick()}
+          href="#!"
+          className={active === "Home" ? style.active : null}
+        >
+          Home
+        </a>
+        <a
+          onClick={() => handleNavClick(projectsRef)}
+          href="#!"
+          className={active === "Projects" ? style.active : null}
+        >
+          Projects
+        </a>
+        <a
+          onClick={() => handleNavClick(skillsRef)}
+          href="#!"
+          className={active === "Skills" ? style.active : null}
+        >
+          Skills
+        </a>
+        <a
+          onClick={() => handleNavClick(contactRef)}
+          href="#!"
+          className={active === "Contact" ? style.active : null}
+        >
+          Contact
+        </a>
+      </div>
+      <div
+        className={show ? `${style.mobile} ${style.rotated}` : style.mobile}
+        onClick={(e) => onClick(e)}
+      >
+        {navSvg}
+      </div>
+      {mobileMenu}
+    </nav>
+  );
 };
 
 Nav.propTypes = {
-	projectsRef: PropTypes.object.isRequired,
-	skillsRef: PropTypes.object.isRequired,
-	contactRef: PropTypes.object.isRequired,
-	fixedNav: PropTypes.bool.isRequired,
-	active: PropTypes.string.isRequired,
+  projectsRef: PropTypes.object.isRequired,
+  skillsRef: PropTypes.object.isRequired,
+  contactRef: PropTypes.object.isRequired,
+  fixedNav: PropTypes.bool.isRequired,
+  active: PropTypes.string.isRequired,
 };
 
 export default Nav;
--- src/components/reusable/logo.js
+++ src/components/reusable/logo.js
@@ -1,69 +1,107 @@
-import React from 'react';
+import React from "react";
 
 export const logo = (
-	<svg xmlns="http://www.w3.org/2000/svg" width="251.23" height="118.82" viewBox="0 0 251.23 118.82">
-		<path
-			d="M324.72,25.54"
-			transform="translate(-63.86 -61.84)"
-			style={{ fill: '#60c', stroke: '#60c', strokeMiterlimit: '10', strokeWidth: '17px' }}
-		/>
-		<path
-			d="M377.53,280.9"
-			transform="translate(-63.86 -61.84)"
-			style={{ fill: '#60c', stroke: '#60c', strokeMiterlimit: '10', strokeWidth: '17px' }}
-		/>
-		<path
-			d="M-191-193.26"
-			transform="translate(-63.86 -61.84)"
-			style={{ fill: '#60c', stroke: '#60c', strokeMiterlimit: '10', strokeWidth: '17px' }}
-		/>
-		<path
-			d="M66.29,143.13H312.36q-5.62,17.76-11.24,35.52L73,177.53Z"
-			transform="translate(-63.86 -61.84)"
-			style={{ fill: '#60c', stroke: '#60c', strokeMiterlimit: '10', strokeWidth: '4px' }}
-		/>
-		<text
-			transform="translate(7.76 83.26) rotate(0.38)"
-			style={{
-				fontSize: '103px',
-				fill: '#006',
-				fontFamily: 'Open Sans, Open Sans',
-				display: 'flex',
-				justifyContent: 'space-between',
-				width: '100%',
-			}}
-		>
-			<tspan>{`{`}</tspan>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="251.23"
+    height="118.82"
+    viewBox="0 0 251.23 118.82"
+  >
+    <path
+      d="M324.72,25.54"
+      transform="translate(-63.86 -61.84)"
+      style={{
+        fill: "#60c",
+        stroke: "#60c",
+        strokeMiterlimit: "10",
+        strokeWidth: "17px",
+      }}
+    />
+    <path
+      d="M377.53,280.9"
+      transform="translate(-63.86 -61.84)"
+      style={{
+        fill: "#60c",
+        stroke: "#60c",
+        strokeMiterlimit: "10",
+        strokeWidth: "17px",
+      }}
+    />
+    <path
+      d="M-191-193.26"
+      transform="translate(-63.86 -61.84)"
+      style={{
+        fill: "#60c",
+        stroke: "#60c",
+        strokeMiterlimit: "10",
+        strokeWidth: "17px",
+      }}
+    />
+    <path
+      d="M66.29,143.13H312.36q-5.62,17.76-11.24,35.52L73,177.53Z"
+      transform="translate(-63.86 -61.84)"
+      style={{
+        fill: "#60c",
+        stroke: "#60c",
+        strokeMiterlimit: "10",
+        strokeWidth: "4px",
+      }}
+    />
+    <text
+      transform="translate(7.76 83.26) rotate(0.38)"
+      style={{
+        fontSize: "103px",
+        fill: "#006",
+        fontFamily: "Open Sans, Open Sans",
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%",
+      }}
+    >
+      <tspan>{`{`}</tspan>
 
-			<tspan style={{ fontSize: '80px' }}>A</tspan>
+      <tspan style={{ fontSize: "80px" }}>A</tspan>
 
-			<tspan>|</tspan>
-			<tspan style={{ fontSize: '80px' }}>R</tspan>
-			<tspan>{`}`}</tspan>
-		</text>
-	</svg>
+      <tspan>|</tspan>
+      <tspan style={{ fontSize: "80px" }}>R</tspan>
+      <tspan>{`}`}</tspan>
+    </text>
+  </svg>
 );
 
 export const logo2 = (
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 339.98 313.46">
-		<rect x="27.08" y="44.64" width="298.13" height="217.07" style={{ fill: '#1b1464' }} />
-		<rect
-			x="368.15"
-			y="508.81"
-			width="275.41"
-			height="202.94"
-			transform="translate(-573.23 -118.86) rotate(-30)"
-			style={{ fill: '#60c' }}
-		/>
-		<path
-			d="M438.54,550H460.9l33.94,98H474l-7.87-23.46H427.36l-8,23.46H404.45ZM462,612.05l-15-45.27-15.32,45.27Z"
-			transform="translate(-335.86 -453.55)"
-			style={{ fill: '#fff' }}
-		/>
-		<path
-			d="M531.51,550H574.7c16,0,29.95,10.62,29.95,29,0,4.69-1.52,11.59-4.83,16.42H585.19V578.93c0-10.08-5.11-16.56-13.8-16.56H550.83v33h34.36l25.39,52.58H589.74l-19.18-40.16H550.83v40.16H531.51Z"
-			transform="translate(-335.86 -453.55)"
-			style={{ fill: '#fff' }}
-		/>
-	</svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 339.98 313.46">
+    <rect
+      x="27.08"
+      y="44.64"
+      width="298.13"
+      height="217.07"
+      style={{ fill: "#1b1464" }}
+    />
+    <rect
+      x="368.15"
+      y="508.81"
+      width="275.41"
+      height="202.94"
+      transform="translate(-573.23 -118.86) rotate(-30)"
+      style={{ fill: "#60c" }}
+    />
+    <path
+      d="M438.54,550H460.9l33.94,98H474l-7.87-23.46H427.36l-8,23.46H404.45ZM462,612.05l-15-45.27-15.32,45.27Z"
+      transform="translate(-335.86 -453.55)"
+      style={{ fill: "#fff" }}
+    />
+    <path
+      d="M531.51,550H574.7c16,0,29.95,10.62,29.95,29,0,4.69-1.52,11.59-4.83,16.42H585.19V578.93c0-10.08-5.11-16.56-13.8-16.56H550.83v33h34.36l25.39,52.58H589.74l-19.18-40.16H550.83v40.16H531.51Z"
+      transform="translate(-335.86 -453.55)"
+      style={{ fill: "#fff" }}
+    />
+  </svg>
+);
+
+export const logo3 = (
+  <img
+    src={`https://res.cloudinary.com/snackmanproductions/image/upload/v1617304425/portfolio/personal-logo_fal5it.png`}
+    alt="logo"
+  />
 );
